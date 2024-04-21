6ee2d682472fa7adfcdc8673ae84098e3a118563
Iesha Osias <44386027+Queen-iSha@users.noreply.github.com>
2024-04-21 11:36:47 -0400

    adding sample graphql schema and files

---
+++ src/main/java/com/kikawrr/MinistriesApplication.java
@@ -0,0 +1,13 @@
+package com.kikawrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinistriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinistriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kikawrr/ministries/MinistriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kikawrr.ministries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinistriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
